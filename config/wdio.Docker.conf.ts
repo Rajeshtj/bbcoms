c946e9a157fcc935ced48718ebde5d2a72967286
“rtj” <rajeshkumar.tj@blackboard.com>
2022-08-11 09:37:52 +0530

    “test17”

--- config/wdio.Docker.conf.ts
+++ config/wdio.Docker.conf.ts
@@ -54,15 +54,15 @@ export const config: WebdriverIO.Config = {
     specFileRetries: 0,
     specFileRetriesDelay: 0,
     specFileRetriesDeferred: false,
-    reporters: [
-    [video, {
-      saveAllVideos: true,       // If true, also saves videos for successful test cases
-      videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
-    }],
+    reporters: ['spec',
+    // [video, {
+    //   saveAllVideos: true,       // If true, also saves videos for successful test cases
+    //   videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
+    // }],
     ['allure', {
       outputDir: './results/allure-raw',
-      disableWebdriverStepsReporting: true,
-      disableWebdriverScreenshotsReporting: true,
+      disableWebdriverStepsReporting: false,
+      disableWebdriverScreenshotsReporting: false,
     }],
   ],
     
